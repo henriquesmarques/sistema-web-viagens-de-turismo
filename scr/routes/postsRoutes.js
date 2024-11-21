6c9674b8f97277ae08493acb701fcfce9935f62a
henriquesmarques <henrique.silveira@estudante.ufjf.br>
2024-11-21 12:13:42 -0300

    Adiciona código inicial do servidor Express

--- scr/routes/postsRoutes.js
+++ scr/routes/postsRoutes.js
@@ -1,12 +1,32 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+
+// Configura armazenamento de arquivos em 'uploads/' no sistema de arquivos do Windows
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ dest: "./uploads", storage });
 
 const routes = (app) => {
-    // Permite que o servidor interprete requisições com corpo no formato JSON
+    // Habilita interpretação de JSON em requisições
     app.use(express.json());
 
-    // Rota para buscar todos os posts dentro do banco de dados
+    // Rota para buscar todos os posts
     app.get("/posts", listarPosts);
+
+    // Rota para criar um novo post
+    app.post("/posts", postarNovoPost);
+
+    // Rota para upload de imagem única (campo 'imagem')
+    app.post("/upload", upload.single("imagem"), uploadImagem);
 };
 
 export default routes;
